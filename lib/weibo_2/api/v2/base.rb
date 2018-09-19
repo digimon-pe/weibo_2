b010d7b98a204170a8b02e1ce715b2fa341c1850
umemotoryo <umemoto0722@gmail.com>
2018-09-19 15:35:12 +0900

    nail care

--- lib/weibo_2/api/v2/base.rb
+++ lib/weibo_2/api/v2/base.rb
@@ -7,41 +7,43 @@ module WeiboOAuth2
     module V2
       class Base
         extend Forwardable
-        
+
         def_delegators :@access_token, :get, :post, :put, :delete
-        
-        @@API_VERSION = 2
-        
+
+        API_VERSION = 2
+
         def initialize(access_token)
           @access_token = access_token
         end
-        
+
         def hashie(response)
           json_body = MultiJson.decode(response.body)
           if json_body.is_a? Array
-            Array.new(json_body.count){|i| Hashie::Mash.new(json_body[i])}
+            Array.new(json_body.count) { |i| Hashie::Mash.new(json_body[i]) }
           else
             Hashie::Mash.new json_body
           end
         end
-        
+
         protected
+
         def mime_type(file)
-          case
-            when file =~ /\.jpeg$/i then 'image/jpeg'
-            when file =~ /\.jpg$/i then 'image/jpeg'
-            when file =~ /\.gif$/i then 'image/gif'
-            when file =~ /\.png$/i then 'image/png'
-            when file =~ /\.tiff$/i then 'image/tiff'
-            else 'image/jpeg'
+          case file
+          when /\.jpeg$/i then 'image/jpeg'
+          when /\.jpg$/i then 'image/jpeg'
+          when /\.gif$/i then 'image/gif'
+          when /\.png$/i then 'image/png'
+          when /\.tiff$/i then 'image/tiff'
+          else 'image/jpeg'
           end
         end
 
         def bin_encode(chunk)
           chunk.force_encoding(Encoding::BINARY) if chunk.respond_to? :force_encoding
         end
-        
-        CRLF = "\r\n"
+
+        CRLF = "\r\n".freeze
+
         def build_multipart_bodies(parts, opts)
           boundary = Time.now.to_i.to_s(16)
 
@@ -56,23 +58,21 @@ module WeiboOAuth2
 
               body << bin_encode("Content-Disposition: form-data; name=\"#{esc_key}\"; filename=\"#{filename}\"#{CRLF}")
               body << bin_encode("Content-Type: #{mime_type}#{CRLF}")
-              body << bin_encode("Content-Transfer-Encoding: binary#{CRLF*2}")
+              body << bin_encode("Content-Transfer-Encoding: binary#{CRLF * 2}")
               body << bin_encode(value.read)
             else
-              body << bin_encode("Content-Disposition: form-data; name=\"#{esc_key}\"#{CRLF*2}#{value}")
+              body << bin_encode("Content-Disposition: form-data; name=\"#{esc_key}\"#{CRLF * 2}#{value}")
             end
             body << bin_encode(CRLF)
+          end
 
-          end 
-
-          body << bin_encode("--#{boundary}--#{CRLF*2}")
+          body << bin_encode("--#{boundary}--#{CRLF * 2}")
 
           {
-            :body => body,
-            :headers => {"Content-Type" => "multipart/form-data; boundary=#{boundary}"}
+            body: body,
+            headers: { 'Content-Type' => "multipart/form-data; boundary=#{boundary}" }
           }
         end
-        
       end
     end
   end
