6856c8991fbb92f3dd529ad4e246a2488f43b747
umemotoryo <umemoto0722@gmail.com>
2018-09-20 12:53:45 +0900

    add weibo_2/client tests

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
 require 'coveralls'
+require 'vcr'
+require 'webmock/rspec'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
@@ -9,6 +11,14 @@ SimpleCov.formatters = [
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/vendor/'
+end
+
+WebMock.disable_net_connect!(allow: 'coveralls.io')
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures'
+  config.allow_http_connections_when_no_cassette = true
+  config.hook_into :webmock
 end
 
 require 'weibo_2'
